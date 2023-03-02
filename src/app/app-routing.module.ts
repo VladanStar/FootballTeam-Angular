baf7f44ef2c054a43cc6a62c6fae8663196dd962
VladanStar <vladancupric@gmail.com>
2023-03-02 10:48:49 +0100

    Add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { TeamComponent } from './pages/team/team.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:'',component:HomeComponent},
+{path:'team',component:TeamComponent},
+{path:'about', component:AboutComponent},
+{path:'contact', component:ContactComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
