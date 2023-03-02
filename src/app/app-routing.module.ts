2ab65e5708fe311a96ff9bbcbeea6ba1eed54301
VladanStar <vladancupric@gmail.com>
2023-03-02 16:31:26 +0100

    Add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,13 @@ import { HomeComponent } from './pages/home/home.component';
 import { TeamComponent } from './pages/team/team.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { TeampageComponent } from './pages/teampage/teampage.component';
 
 
 const routes: Routes = [
 {path:'',component:HomeComponent},
 {path:'team',component:TeamComponent},
-
+{path:'team/:id',component:TeampageComponent},
 {path:'about', component:AboutComponent},
 {path:'contact', component:ContactComponent},
 
--- src/app/pages/teampage/teampage.component.ts
+++ src/app/pages/teampage/teampage.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PlayersDetailService } from 'src/app/services/players-detail.service';
 
 @Component({
   selector: 'app-teampage',
   templateUrl: './teampage.component.html',
   styleUrls: ['./teampage.component.css']
 })
-export class TeampageComponent {
+export class TeampageComponent  implements OnInit{
+  getTeamId:any;
+  teamData:any;
+
+constructor(private param:ActivatedRoute, private service:PlayersDetailService){}
+
+ngOnInit(): void {
+  this.getTeamId = this.param.snapshot.paramMap.get('id');
+  console.log(this.getTeamId,'getteam');
+if(this.getTeamId){
+  this.teamData =  this.service.playersDetails.filter((value)=>{
+    return value.id == this.getTeamId;
+  });
+  console.log(this.teamData,'teamdata>>');
+}
+}
 
 }
