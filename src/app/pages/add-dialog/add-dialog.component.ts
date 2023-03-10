f1765cb5d106f8eac7ca1edf70d37b6554cc681b
VladanStar <vladancupric@gmail.com>
2023-03-10 09:49:58 +0100

    Add

--- src/app/pages/add-dialog/add-dialog.component.ts
+++ src/app/pages/add-dialog/add-dialog.component.ts
@@ -5,30 +5,30 @@ import { PlayersDetailService } from 'src/app/services/players-detail.service';
 @Component({
   selector: 'app-add-dialog',
   templateUrl: './add-dialog.component.html',
-  styleUrls: ['./add-dialog.component.css']
+  styleUrls: ['./add-dialog.component.css'],
 })
 export class AddDialogComponent implements OnInit {
   playerForm!: FormGroup;
 
+  uneseniTekst: any;
+  ime: string = '';
+  poruka: string = '';
+  profil: string = '';
+  message: string = '';
+  empList: Array<{ profil: string; message: string }> = [];
 
-
-  uneseniTekst:any;
-  ime:string='';
-  poruka:string="";
-  profil:string="";
-    message :string="";
-    empList: Array<{profil: string, message: string}> = [];
-
-ngOnInit(): void {
-  this.playerForm = new FormGroup({
-    'playerName':new FormControl(null, [Validators.required]),
-    'international': new FormControl(null, [Validators.required]),
-    'transfer':new FormControl(null, Validators.required),
-    'content':new FormControl('write text',[ Validators.required, Validators.maxLength(250)])
+  ngOnInit(): void {
+    this.playerForm = new FormGroup({
+      playerName: new FormControl(null, [Validators.required]),
+      international: new FormControl(null, [Validators.required]),
+      transfer: new FormControl(null, Validators.required),
+      content: new FormControl('write text', [
+        Validators.required,
+        Validators.maxLength(250),
+      ]),
     });
-
-}
-addPlayer(){
-console.log(this.playerForm.value)
-}
+  }
+  addPlayer() {
+    console.log(this.playerForm.value);
+  }
 }
--- src/app/pages/fans-wall/fans-wall.component.ts
+++ src/app/pages/fans-wall/fans-wall.component.ts
@@ -5,36 +5,39 @@ import { ForbiddenNameValidator } from './custom-validators';
 @Component({
   selector: 'app-fans-wall',
   templateUrl: './fans-wall.component.html',
-  styleUrls: ['./fans-wall.component.css']
+  styleUrls: ['./fans-wall.component.css'],
 })
-export class FansWallComponent  implements OnInit{
+export class FansWallComponent implements OnInit {
   projectForm!: FormGroup;
-  uneseniTekst:any;
-  ime:string='';
-  poruka:string="";
-  profil:string="";
-    message :string="";
-    empList: Array<{profil: string, message: string}> = [];
 
+  ime: string = '';
+  poruka: string = '';
+  profil: string = '';
+  message: string = '';
+  empList: Array<{ profil: string; message: string }> = [];
 
-
-  messageArray:Array<{user:string,message:string}> = [];
+  messageArray: Array<{ user: string; message: string }> = [];
 
   ngOnInit(): void {
-  this.projectForm = new FormGroup({
-  'projectName':new FormControl(null, [Validators.required, ForbiddenNameValidator(/Test/)]),
-  'email': new FormControl(null, [Validators.required, Validators.email]),
-  'projectStatus':new FormControl('Fans'),
-  'content':new FormControl('write text',[ Validators.required, Validators.maxLength(250)])
-  });
+    this.projectForm = new FormGroup({
+      projectName: new FormControl(null, [
+        Validators.required,
+        ForbiddenNameValidator(/Test/),
+      ]),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      projectStatus: new FormControl('Fans'),
+      content: new FormControl('write text', [
+        Validators.required,
+        Validators.maxLength(250),
+      ]),
+    });
   }
-  onSaveProject(){
-
-  console.log(this.projectForm.value)
-  this.ime = this.projectForm.value.projectName;
-  this.poruka = this.projectForm.value.content;
-  console.log(this.profil,this.message);
-  this.empList.unshift({ profil: this.ime, message: this.poruka });
-this.projectForm.reset();
+  onSaveProject() {
+    console.log(this.projectForm.value);
+    this.ime = this.projectForm.value.projectName;
+    this.poruka = this.projectForm.value.content;
+    console.log(this.profil, this.message);
+    this.empList.unshift({ profil: this.ime, message: this.poruka });
+    this.projectForm.reset();
   }
 }
