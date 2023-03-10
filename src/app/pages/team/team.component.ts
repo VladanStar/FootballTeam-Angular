d425b753a5a33089001e5645914d21991c73318a
VladanStar <vladancupric@gmail.com>
2023-03-10 06:35:04 +0100

    Add

--- src/app/pages/team/team.component.ts
+++ src/app/pages/team/team.component.ts
@@ -9,9 +9,14 @@ import { PlayersDetailService } from 'src/app/services/players-detail.service';
 export class TeamComponent implements OnInit {
 searchText:any;
 playerData:any;
+
 constructor(private service:PlayersDetailService){}
 ngOnInit(): void {
   this.playerData=this.service.playersDetails;
 }
-
+deleteItem(i:any){
+console.log(i);
+this.playerData.splice(i,1)
+console.log(this.playerData)
+}
 }
