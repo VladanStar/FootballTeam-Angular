610c7379957609800239fa0c1a9069ddeff0694d
VladanStar <vladancupric@gmail.com>
2023-03-10 10:14:53 +0100

    Add

--- src/app/pages/add-dialog/add-dialog.component.ts
+++ src/app/pages/add-dialog/add-dialog.component.ts
@@ -8,7 +8,10 @@ import { PlayersDetailService } from 'src/app/services/players-detail.service';
   styleUrls: ['./add-dialog.component.css'],
 })
 export class AddDialogComponent implements OnInit {
+
+constructor(private service:PlayersDetailService ){}
   playerForm!: FormGroup;
+  playerData:any;
 
   uneseniTekst: any;
   ime: string = '';
@@ -16,19 +19,23 @@ export class AddDialogComponent implements OnInit {
   profil: string = '';
   message: string = '';
   empList: Array<{ profil: string; message: string }> = [];
-
+player:any={ime:'Vladan Cupric', int:"Serbian"}
   ngOnInit(): void {
+this.playerData=this.service.playersDetails;
     this.playerForm = new FormGroup({
       playerName: new FormControl(null, [Validators.required]),
       international: new FormControl(null, [Validators.required]),
       transfer: new FormControl(null, Validators.required),
       content: new FormControl('write text', [
         Validators.required,
-        Validators.maxLength(250),
+        Validators.maxLength(150),
       ]),
     });
   }
   addPlayer() {
     console.log(this.playerForm.value);
+this.playerData=this.playerData.push(this.player)
+console.log(this.playerData)
+this.playerForm.reset();
   }
 }
