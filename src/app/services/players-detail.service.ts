35708d4657f81e74b9c5614dc91099428f9ac338
VladanStar <vladancupric@gmail.com>
2023-03-20 12:27:04 +0100

    Add

--- src/app/services/players-detail.service.ts
+++ src/app/services/players-detail.service.ts
@@ -97,7 +97,7 @@ playersDetails = [
     international:"Belgium",
     playerImg:"https://img.a.transfermarkt.technology/portrait/header/108390-1665067957.jpg?lm=1"
   },{
-    id:1,
+    id:12,
     playerName:"Kylian Mbappé",
     playerDetails:"Winner of Golden Boy.  Award - 2017. Winner of French Player of the Year Award - 2018.",
     playerTransferMarket:180.00,
@@ -105,7 +105,7 @@ playersDetails = [
     playerImg:"https://img.a.transfermarkt.technology/portrait/header/342229-1632686046.jpg?lm=1"
   },
   {
-    id:2,
+    id:13,
     playerName:"Erling Haaland",
     playerDetails:"Most goals scored by a player in a single match at a FIFA U-20 World Cup (9 goals)",
     playerTransferMarket:170.00,
@@ -113,7 +113,7 @@ playersDetails = [
     playerImg:"https://img.a.transfermarkt.technology/portrait/header/418560-1656179352.jpg?lm=1"
   },
   {
-    id:3,
+    id:14,
     playerName:"Vinicius Junior",
     playerDetails:"Main position:Left Winger Other position:Right Winger",
     playerTransferMarket:120,
@@ -121,7 +121,7 @@ playersDetails = [
     playerImg:"https://img.a.transfermarkt.technology/portrait/header/371998-1664869583.jpg?lm=1"
   },
   {
-    id:4,
+    id:15,
     playerName:"Phil Foden",
     playerDetails:"Reddish Vulcans, Manchester City (2009-2017)",
     playerTransferMarket:110,
@@ -129,7 +129,7 @@ playersDetails = [
     playerImg:"https://img.a.transfermarkt.technology/portrait/header/406635-1668524492.jpg?lm=1"
   },
   {
-    id:5,
+    id:16,
     playerName:"Jude Bellingham",
     playerDetails:"Youngest ever player to feature for Birmingham City at the age of 16 years and 38 days old.",
     playerTransferMarket:110,
@@ -137,14 +137,14 @@ playersDetails = [
     playerImg:"https://img.a.transfermarkt.technology/portrait/header/581678-1661412719.jpg?lm=1"
   },
   {
-    id:6,
+    id:17,
     playerName:"Pedri",
     playerDetails:"Main position:Central Midfield",
     playerTransferMarket:100,
     international:"Spain",
     playerImg:"https://img.a.transfermarkt.technology/portrait/header/683840-1620304992.jpg?lm=1"
   }, {
-    id:7,
+    id:18,
     playerName:"Jamal Musiala",
     playerDetails:"TSV Lehnerz (2008-2010), Southampton FC (2010-2011), Chelsea FC (2011-2019), FC Bayern München (2019-2020)",
     playerTransferMarket:100,
@@ -152,7 +152,7 @@ playersDetails = [
     playerImg:"https://img.a.transfermarkt.technology/portrait/header/580195-1667830802.jpg?lm=1"
   },
   {
-    id:8,
+    id:19,
     playerName:"Bukayo Saka",
     playerDetails:"Greenford Celtic, Watford FC (-2008), Arsenal FC (2008-2019)",
     playerTransferMarket:100,
@@ -160,7 +160,7 @@ playersDetails = [
     playerImg:"https://img.a.transfermarkt.technology/portrait/header/433177-1669294809.jpg?lm=1"
   },
   {
-    id:9,
+    id:20,
     playerName:"Federico Valverde",
     playerDetails:"Club Estudiantes (La Unión), Exploradores (Artigas), Siete Estrellas (2008-2010), Carabelas (2011)",
     playerTransferMarket:100,
@@ -168,7 +168,7 @@ playersDetails = [
     playerImg:"https://img.a.transfermarkt.technology/portrait/header/369081-1581501132.jpg?lm=1"
   },
   {
-    id:10,
+    id:21,
     playerName:"Pablo Gavira Gavi",
     playerDetails:"Main position:Central Midfield",
     playerTransferMarket:90,
@@ -176,7 +176,7 @@ playersDetails = [
     playerImg:"https://img.a.transfermarkt.technology/portrait/header/646740-1653039221.jpg?lm=1"
   },
   {
-    id:11,
+    id:22,
     playerName:"Thibaut Courtois",
     playerDetails:"Bilzen VV (1997–1999), KRC Genk (1999-2009)",
     playerTransferMarket:60,
